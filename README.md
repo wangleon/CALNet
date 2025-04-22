2f3b273580048c0253fd03a21ea5e8b4c7e6ddfd
Liang Wang <wang.leon@gmail.com>
2025-04-22 17:43:23 +0800

    change file structure. update README

--- README.md
+++ README.md
@@ -1,41 +1,24 @@
 # CALNet
-CNN Attention LSTM Net, a hybrid deep learning model to identify eclipsing binaries from TESS light curves
+CNN Attention LSTM Net, a hybrid deep learning model to identify eclipsing binaries from TESS light curves.
 
-TESS-EBs/<br>
+## About TESS
+The Transiting Exoplanet Survey Satellite (TESS) is a space telescope designed to search for exoplanets using
+the transit method. As of Apirl 2024, TESS has scanned over 85% of the entire sky, and released 2-minute cadence
+light curves for about 530,000 stars. Besides the light curves, TESS also releases the Full-Frame Images (FFIs)
+with the sampling of 30 minutes.
 
-├── data/                        
+## Data Preparation
+Before using this machine learning model, users can download all the 2-minute cadence light curves from the
+[MAST archive](https://archive.stsci.edu/tess/bulk_downloads/bulk_downloads_ffi-tp-lc-dv.html), and we assume
+all downloaded files are stored in the following path
 
-	├── rawData/                # our EB samples  
-	├── preprocessing.py/       # preprocessing method
-	|── preprocessing_imply.py  # how to use our preprocessing method
- 	|—— periodogram.py	    # GLS method
+        tess/lc/s{DDD}/{SSSSS}.fits
 
+where `DDD` is the zero-padded 3-digit sector number (e.g., `s015` for Sector 15), and `SSSSS.fits` is the
+filename of the light curve data in FITS format. This path can be changed in `paths.json`.
 
+## Training Data
+The list of TICs as input of training data can be found in `data/training_samples.dat`.
 
-├── model/  
+## Model Architecture
 
-	├── CALNet.py/              # our model -- CALNet structure and how to train
-
-
-
-├── predict/  
-
-	├── predict.py              # predict method
-	|── predict_imply.py        # imply predict
-
-
-├── outputs/  
-
-	├── images                  # some EB examples we predict
-
-
-├── .gitignore      
-
-
-├── requirements.txt      
-
-
-├── LICENSE      
-
-
-└── README.md                
--- preprocessing_imply.py
+++ preprocessing_imply.py
@@ -1,41 +1,43 @@
-import preprocessing
+import os
+import json
+import preprocessing as prep
 import periodogram #GLS
 
 
-def getProcessedData(lc_pre_loc, lc_pre_save_loc, gls_pre_save_loc, sec, gap_width, outlier_cut, flux_len, period_lst):
+def getProcessedData(lc_pre_loc, lc_pre_save_loc, gls_pre_save_loc,
+        sector, gap_width, outlier_cut, flux_len, period_lst):
     '''
     get preprocessed lc and gls data and save them in the file.
     Args:
         lc_pre_loc: The file path prefix where the raw lc stored.
         lc_pre_save_loc: The file path prefix where the processed lc will be stored.
         gls_pre_save_loc: The file path prefix where the processed gls will be stored.
-        sec:sector name
+        sector (int): sector number
         gap_with: when the gap reaches the gap_width, we split the light curve.
         outlier_cut:The spline residuals before the data point with the maximum standard deviation from the median are considered outliers.
         flux_len: the length of the light curve we set.
         period_lst: The x-axis of the GLS data represents the frequency.
     '''
-    # The file location where the data will be stored.
-    lc_csv_file = lc_pre_save_loc + sec + '.csv'
-    GLS_csv_file = gls_pre_save_loc + sec + '.csv'
+    # The file location where the data is stored.
+    lc_csv_file = os.path.join(lc_pre_save_loc, 's{:03d}.csv'.format(sector))
+    GLS_csv_file = os.path.join(gls_pre_save_loc, 's{:03d}.csv'.format(sector))
     
     # get lc files
-    dir = lc_pre_loc + sec + '/'
-    fileLst = os.listdir(dir)
-    for file in fileLst:
-        filename = dir + file
+    path = os.path.join(lc_pre_loc, 's{:03d}'.format(sector))
+    for fname in os.listdir(path):
+        filename = os.path.join(path, fname)
         # split process
-        out_time,out_flux = lc_split(filename,gap_width)
+        out_time,out_flux = prep.lc_split(filename, gap_width)
         for t,f in zip(out_time,out_flux):
             # we abandon the flux whose length < 2000
             if len(t) >= 2000:
                 # dropMax process
-                t,f = dropMax(t,f,99)
+                t,f = prep.dropMax(t,f,99)
                 # preprocessing of lc
-                final_flux = process_lc(t,f,outlier_cut,flux_len)
-                save_flux = [file]+list(final_flux)
+                final_flux = prep.process_lc(t,f,outlier_cut,flux_len)
+                save_flux = [fname]+list(final_flux)
                 # save into file
-                writeData(save_flux,lc_csv_file)
+                prep.writeData(save_flux,lc_csv_file)
                 
                 # GLS operation, get power
                 pdm = periodogram.GLS(t, f)
@@ -44,20 +46,26 @@ def getProcessedData(lc_pre_loc, lc_pre_save_loc, gls_pre_save_loc, sec, gap_wid
                 p_max = max(power)
                 p_min = min(power)
                 p = (power - p_min)/(p_max - p_min)
-                save_power = [file]+list(p)
+                save_power = [fname]+list(p)
                 # save
-                writeData(save_power,GLS_csv_file)
+                prep.writeData(save_power,GLS_csv_file)
 
 gap_width = 0.75 
 outlier_cut = 3
 flux_len = 4000
 period_lst = np.logspace(-3, 1, 1125)[125:]
 
-lc_pre_loc = "F:/tess/lc/"
-lc_pre_save_loc = 'F:/tess/processedData/lc/'
-gls_pre_save_loc = 'F:/tess/processedData/GLS/'
 
-sectorlst = ['s088']
+f = open('../paths.json')
+PATHS = json.load(f)
+f.close()
 
-for sec in sectorlst:
-    getProcessedData(lc_pre_loc, lc_pre_save_loc, gls_pre_save_loc, sec, gap_width, outlier_cut, flux_len, period_lst)
+
+sector_lst = [88]
+
+for sector in sector_lst:
+    getProcessedData(
+            PATHS['lc_pre_loc'],
+            PATHS['lc_pre_save_loc'],
+            PATHS['gls_pre_save_loc'],
+            sector, gap_width, outlier_cut, flux_len, period_lst)
