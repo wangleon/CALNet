88798335e0f4dd836d1a429c0250b578c2bfbce9
chenjingzlh <55786282+chenjingzlh@users.noreply.github.com>
2025-06-25 16:31:23 +0800

    Add files via upload
    Add code in pytorch version

--- model.py
+++ model.py
@@ -1,90 +1,123 @@
-import pickle
-# import numpy as np
-# import pandas as pd
-import tensorflow as tf
-from keras import metrics
-from keras.layers import *
-from keras.models import Model
-from keras.optimizers import Adam
-# from tensorflow.python.keras.utils import np_utils
-
-def CBAM(x,filter_num,reduction_ratio):
-    # Channel Attention
-    avgpool = GlobalAveragePooling1D()(x)
-    maxpool = GlobalMaxPooling1D()(x)
-    # Shared MLP
-    Dense_layer1 = Dense(filter_num//reduction_ratio, activation='relu')
-    Dense_layer2 = Dense(filter_num, activation='relu')
-    avg_out = Dense_layer2(Dense_layer1(avgpool))
-    max_out = Dense_layer2(Dense_layer1(maxpool))
-
-    channel = Add()([avg_out, max_out])
-    channel = Activation('sigmoid')(channel)
-    channel_out = Multiply()([x, channel])
-
-    # Spatial Attention
-    avgpool = tf.reduce_mean(channel_out, axis=1, keepdims=True)
-    maxpool = tf.reduce_max(channel_out, axis=1, keepdims=True)
-    spatial = Concatenate()([avgpool, maxpool])
-
-    spatial = Conv1D(1, kernel_size=8, padding='SAME')(spatial)
-    spatial_out = Activation('sigmoid',)(spatial)
-
-    CBAM_out = tf.multiply(channel_out, spatial_out)
-    return CBAM_out
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
 
 
-input1 = Input(shape=(4000,1))
-seq1 = input1
+#### Define the CBAM
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.max_pool = nn.AdaptiveMaxPool1d(1)
+        self.fc = nn.Sequential(
+            nn.Conv1d(in_channels, in_channels // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(in_channels // reduction, in_channels, 1, bias=False),
+        )
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
 
-seq1 = Conv1D(64,8, padding='SAME',activation='relu')(seq1)
-seq1 = CBAM(seq1,64,8)
-seq1 = MaxPooling1D(4)(seq1)
-
-seq1 = Conv1D(128,8, padding='SAME',activation='relu')(seq1)
-seq1 = CBAM(seq1,128,8)
-seq1 = MaxPooling1D(4)(seq1)
-
-seq1 = Conv1D(256,8, padding='SAME',activation='relu')(seq1)
-seq1 = CBAM(seq1,256,8)
-seq1 = MaxPooling1D(4)(seq1)
-
-seq1 = LSTM(128, return_sequences=True)(seq1)
-seq1 = Flatten()(seq1)
-
-input2 = Input(shape=(1000,1))
-seq2 = input2
-seq2 = Conv1D(32,8, padding='SAME',activation='relu')(seq2)
-seq2 = CBAM(seq2,32,8)
-seq2 = MaxPooling1D(4)(seq2)
-
-seq2 = Conv1D(64,8, padding='SAME',activation='relu')(seq2)
-seq2 = CBAM(seq2,64,8)
-seq2 = MaxPooling1D(4)(seq2)
-
-seq2 = LSTM(64, return_sequences=True)(seq2)
-seq2 = Flatten()(seq2)
-seq2 = RepeatVector(2)(seq2)
-seq2 = Flatten()(seq2)
-
-add = Add()([seq1,seq2])
-seq = Dense(1024, activation='sigmoid')(add)
-seq = Dropout(0.5)(seq)
-
-output_tensor = Dense(2, activation='softmax')(seq)
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv1 = nn.Conv1d(2, 1, kernel_size=kernel_size, padding=3, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+    
+class CBAM(nn.Module):
+    def __init__(self, in_channels, reduction, kernel_size=7):
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(in_channels, reduction)
+        self.spatial_attention = SpatialAttention(kernel_size)
+    def forward(self, x):
+        x = x * self.channel_attention(x)
+        x = x * self.spatial_attention(x)
+        return x
 
 
-ep = 30  #epoch
-# metrics
-METRICS = [
-    metrics.BinaryAccuracy(name='accuracy'),
-    metrics.Precision(name='precision'),
-    metrics.Recall(name='recall'),
-    metrics.AUC(name='auc'),
-]
+### model structure
+class CNN_CBAM_LSTM_model(nn.Module):
+    def __init__(self):
+        super(CNN_CBAM_LSTM_model, self).__init__()
+        self.maxpool = nn.MaxPool1d(4)
+        self.lstm1 = nn.LSTM(256, 128, 2, batch_first=True) ###(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm2 = nn.LSTM(64, 64, 2, batch_first=True)
+        
+        self.lcbranch = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True),
+            CBAM(64, reduction=8),
+            nn.MaxPool1d(4),
+            nn.Conv1d(64, 128, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True),
+            CBAM(128, reduction=8),
+            nn.MaxPool1d(4),
+            nn.Conv1d(128, 256, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(4),
+        )
+        
+        self.glsbranch = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True),
+            CBAM(32, reduction=8),
+            nn.MaxPool1d(4),
+            nn.Conv1d(32, 64, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True),
+            CBAM(64, reduction=8),
+            nn.MaxPool1d(4),
+        )
+        
+        self.fc2 = nn.Sequential(
+            nn.Linear(7936, 1024),
+            nn.Sigmoid(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 2),
+        )
+        
+    def forward(self, x1, x2):
+        x1 = self.lcbranch(x1)
+        x1 = x1.permute(0, 2, 1)  # Change shape to (batch_size, seq_length, features) for LSTM
+        x1, _ = self.lstm1(x1)
+        x1 = nn.Flatten()(x1)
+        # print("LC Shape after Flatten:", x1.shape)
+        
+        x2 = self.glsbranch(x2)
+        x2 = x2.permute(0, 2, 1)  # Change shape to (batch_size, seq_length, features) for LSTM
+        x2, _ = self.lstm2(x2)
+        x2 = nn.Flatten()(x2)
+        x2 = x2.repeat(1, 2)
+        x2 = nn.Flatten()(x2)
+        # print("GLS Shape after repeat:", x2.shape)
+        
+        ###combine
+        x = x1 + x2
+        x = self.fc2(x)
+        # print("Output Shape:", x.shape)
+        
+        return nn.functional.softmax(x, dim=1)  # Return both softmax output and raw logits
 
-model = Model(inputs=[input1, input2], outputs=output_tensor)
-model.compile(loss='binary_crossentropy',optimizer=Adam(1e-3, amsgrad=True), metrics=METRICS)
-history = model.fit([f_trainX,g_trainX],f_trainY, epochs=ep,validation_data = ([f_testX,g_testX],f_testY), workers=4, use_multiprocessing=True,batch_size = 128)
+if __name__ == "__main__":
+    # Example usage
+    model = CNN_CBAM_LSTM_model()
+    # lcData = torch.randn(128, 1, 4000)  # Example LC data
+    # GLSData = torch.randn(128, 1, 1000)   # Example GLS data
+    # output = model(lcData, GLSData)
+    # print("Output:", output)
+    # print("Output shape:", output.shape)
+
+
+
+
 
 
